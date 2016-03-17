67701496de63dad4737e616ba7b7ec2fb6cd5241
dlevkovits <dlevkovits@gmail.com>
2016-03-17 18:12:54 -0400

    Updated homework

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+  $('#stopButton').click(illuminateRed);
+  $('#slowButton').click(illuminateYellow);
+  $('#goButton').click(illuminateGreen);
+  
+
+  function illuminateRed() {
+    clearLights();
+    $('#stopLight').css("background-color", "red");
+  }
+
+  function illuminateYellow() {
+    clearLights();
+    $('#slowLight').css("background-color", "yellow");
+  }
+
+  function illuminateGreen() {
+    clearLights();
+    $('#goLight').css("background-color", "green");
+  }
+
+  function clearLights() {
+  $('#stopLight').css("background-color", "black");
+  $('#slowLight').css("background-color", "black");
+  $('#goLight').css("background-color", "black");
+}
