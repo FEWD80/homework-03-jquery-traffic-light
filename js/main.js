48a19846f85f463d84de05fc983d8dfeb8f859c4
hlateman <henryspethmann@Henrys-Betamax.local>
2016-03-22 21:23:12 -0400

    Updated everything with jQuery
    primarily changed main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-document.getElementById('stopButton').onclick = illuminateRed;
-document.getElementById('slowButton').onclick = illuminateYellow;
-document.getElementById('goButton').onclick = illuminateGreen;
+$(function(){
+
+$('#stopButton').click(illuminateRed);
+$('#slowButton').click(illuminateYellow);
+$('#goButton').click(illuminateGreen);
+
 
 function illuminateRed() {
   clearLights();
-  document.getElementById('stopLight').style.backgroundColor = "red";
+  $('#stopLight').css('backgroundColor', 'red');
 }
 
 function illuminateYellow() {
   clearLights();
-  document.getElementById('slowLight').style.backgroundColor = "yellow";
+  $('#slowLight').css('backgroundColor', 'yellow');
 }
 
 function illuminateGreen() {
   clearLights();
-  document.getElementById('goLight').style.backgroundColor = "green";
+  $('#goLight').css('backgroundColor', 'green');
 }
 
 function clearLights() {
-  document.getElementById('stopLight').style.backgroundColor = "black";
-  document.getElementById('slowLight').style.backgroundColor = "black";
-  document.getElementById('goLight').style.backgroundColor = "black";
+  $('#stopLight').css('backgroundColor', 'black');
+  $('#slowLight').css('backgroundColor', 'black');
+  $('#goLight').css('backgroundColor', 'black');
 }
+
+});
